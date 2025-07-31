8a94dd882da8a3cc64aa2f3bb88e5e82947ec167
柯科 <keke@ky-tech.com.cn>
2025-07-31 10:45:26 +0800

    update example coding

--- lib/muyu/muyu_page.dart
+++ lib/muyu/muyu_page.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_answer_questions/muyu/count_panel.dart';
+import 'package:flutter_answer_questions/muyu/muyu_assets_image.dart';
 
 class MuyuPage extends StatefulWidget {
   const MuyuPage({super.key});
@@ -10,44 +14,39 @@ class MuyuPage extends StatefulWidget {
 }
 
 class _MuyuPageState extends State<MuyuPage> {
+
+  int _counter = 0;
+  final Random random = Random();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(" muyu"),
-        titleTextStyle: const TextStyle(color: Colors.black,fontSize: 16,fontWeight: FontWeight.bold),
+        titleTextStyle: const TextStyle(
+            color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold),
         backgroundColor: Colors.white,
         iconTheme: const IconThemeData(color: Colors.black),
         elevation: 0,
         actions: [
-          IconButton(onPressed: _toHistory,icon: const Icon(Icons.history))
+          IconButton(onPressed: _toHistory, icon: const Icon(Icons.history))
         ],
       ),
       body: Column(
         children: [
-          Expanded(child: _buildTopContent()),
-          // Expanded(child: _buildImage()),
+          Expanded(child: CountPanel(count: _counter, onTapSwitchAudio: () {}, onTapSwitchImage: () {},)),
+          Expanded(child: MuyuAssetsImage(image: 'assets/images/muyu.png',onTap: _onKnock,)),
         ],
       ),
     );
   }
-
-  Widget _buildTopContent() {
-    return const Stack(
-      children: [
-        Center(
-          child: Text(
-            '功德数：0',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        )
-      ],
-    );
+  void _onKnock() {
+    print("onTap:$_counter");
+    setState(() {
+      int addCount = 1 + random.nextInt(3);
+      _counter += addCount;
+    });
   }
 
   void _toHistory() {}
-
 }
